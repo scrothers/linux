e7bd902e10b7e13ebacc0960504fbc49bae697d1
Steven Crothers <steven@layerworx.com>
2015-08-30 20:48:10 -0400

    Making some more adjustments to the source installer, now functionally complete

--- UTILS/installsource.sh
+++ UTILS/installsource.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd )
 
 # Test for spectool on the system
 /usr/bin/which spectool > /dev/null 2> /dev/null
@@ -8,7 +8,12 @@ if [ $? -eq 0 ]; then
 	for SPEC in ${DIR}/SPECS/*; do
 		echo "[+] Downloading sources for ${SPEC}..."
 		spectool -g -R ${SPEC}
+		if [ $? -ne 0 ]; then
+			echo "Error: Unable to download source for ${SPEC}..."
+			exit 1
+		fi
 	done
 else
 	echo "Error: Unable to locate 'spectool' which is required to download all sources."
+	exit 1
 fi
